c3361ceeaa24bc0f065ad14c0544b9197608056a
Tara Sherman <taradelin@gmail.com>
2019-09-08 11:56:08 -0400

    imported Route component in App.js and set up routes to components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+// imported Route component
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
   <div>
-    <Navigation />
+    <Route exact path='/' component={Home} />
+    <Route path='/About' component={About} />
+    <Route path='/Contact' component={Contact} />
+    <Route path='/Navigation' component={Navigation} />
   </div>
 );
-
+// set up routes to components
 export default App;
