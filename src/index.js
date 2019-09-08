6141fc49b5f330b5ca0055ea6ffa721743fb3df3
Tara Sherman <taradelin@gmail.com>
2019-09-08 11:54:57 -0400

    imported BrowserRouter as Router, wrapped App component inside Router component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+// imported BrowserRouter as Router
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+  <App />
+  </Router>,
+  document.getElementById('root'));
+// wrapped App component inside Router component
